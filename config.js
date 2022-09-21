1ae2b53be8795a0a7b7b9c1175399642ec258211
nicolascastrocai <nicolascastrocai@gmail.com>
2022-09-21 16:59:05 -0300

    git

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-    PORT : 8080,
+    PORT : 808,
     TIPO_DE_PERSISTENCIA: 'MONGODB',    // 'MEM', 'FILE', 'MONGODB'
     //STR_CNX: 'mongodb://localhost/ecommerce'
     STR_CNX: 'mongodb+srv://nicolas:nicolas123456.@cluster0.fvlkz.mongodb.net/comercio?retryWrites=true&w=majority'
